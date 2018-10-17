cdd06d0de8bb6b711263c6bf2ad880263eb32c0d
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-17 12:45:43 +0200

    Items hinzugefügt

--- Advanced_Kniffel/src/Models/KniffelTableModel.java
+++ Advanced_Kniffel/src/Models/KniffelTableModel.java
@@ -12,6 +12,22 @@ public class KniffelTableModel extends AbstractTableModel {
     private ArrayList<KniffelRow> rows = new ArrayList<>();
     private static final String[] COLS = {"Spiel", "Wahl", "Punkte"};
 
+    public KniffelTableModel() {
+        rows.add(new KniffelRow("Nur Einser", Boolean.FALSE, 0));
+         rows.add(new KniffelRow("Nur Zweier", Boolean.FALSE, 0));
+          rows.add(new KniffelRow("Nur Dreier", Boolean.FALSE, 0));
+           rows.add(new KniffelRow("Nur Vierer", Boolean.FALSE, 0));
+            rows.add(new KniffelRow("Nur Fünfer", Boolean.FALSE, 0));
+             rows.add(new KniffelRow("Nur Sechser", Boolean.FALSE, 0));
+              rows.add(new KniffelRow("Pasch 3", Boolean.FALSE, 0));
+               rows.add(new KniffelRow("Pasch 4", Boolean.FALSE, 0));
+                rows.add(new KniffelRow("Full House", Boolean.FALSE, 0));
+                 rows.add(new KniffelRow("Straße klein", Boolean.FALSE, 0));
+                  rows.add(new KniffelRow("Straße groß", Boolean.FALSE, 0));
+                   rows.add(new KniffelRow("Kniffel(nur 5er)", Boolean.FALSE, 0));
+                    rows.add(new KniffelRow("Chane", Boolean.FALSE, 0));
+    }
+
     @Override
     public int getRowCount() {
         return rows.size();
@@ -26,11 +42,11 @@ public class KniffelTableModel extends AbstractTableModel {
     public Object getValueAt(int i, int i1) {
         KniffelRow r = rows.get(i);
         switch (i1) {
-            case 1:
+            case 0:
                 return r.getText();
-            case 2:
+            case 1:
                 return r.getWahl();
-            case 3:
+            case 2:
                 return r.getPunkte();
             default:
                 return "???";
