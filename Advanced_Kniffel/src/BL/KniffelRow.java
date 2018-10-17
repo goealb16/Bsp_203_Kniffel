f0fddb1883fe81e61494ddca7d91cf4735f7eab3
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-17 10:19:47 +0200

    KniffelRow Klasse Konstruktor + Getter erstellt.

--- Advanced_Kniffel/src/BL/KniffelRow.java
+++ Advanced_Kniffel/src/BL/KniffelRow.java
@@ -1,9 +1,31 @@
 /*
 #ProProgrammer
-*/
+ */
 package BL;
+
 
 
 public class KniffelRow {
 
+    private String text;
+    private  Boolean wahl;
+
+    public String getText() {
+        return text;
+    }
+
+    public Boolean getWahl() {
+        return wahl;
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+
+    public KniffelRow(String text, Boolean wahl, int punkte) {
+        this.text = text;
+        this.wahl = wahl;
+        this.punkte = punkte;
+    }
+    private int punkte;
 }
